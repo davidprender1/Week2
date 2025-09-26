78f94a734c07f6ee8b2cd13e94275385b8ed8f00
David Prendergast <g00405007@atu.ie>
2025-09-26 10:42:01 +0100

    Updated Pom and added new class RequestController

---
+++ src/main/java/com/example/week2/RequestController.java
@@ -0,0 +1,17 @@
+package com.example.week2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class RequestController {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello World";
+    }
+
+}
