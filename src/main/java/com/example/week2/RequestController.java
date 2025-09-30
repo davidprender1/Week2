829401f5a68ca811b2d28465d9d2e5141ff99cca
DAVIDPRENDERGAST-STU <g00405007@atu.ie>
2025-09-30 17:05:45 +0100

    Added calculator

--- src/main/java/com/example/week2/RequestController.java
+++ src/main/java/com/example/week2/RequestController.java
@@ -27,4 +27,37 @@ public class RequestController {
     public Person getperson() {
         return new Person("David", 22);
     }
+
+    @GetMapping("/calculate")
+    public Map<String, Object> calculate(
+            @RequestParam double num1,
+            @RequestParam double num2,
+            @RequestParam String operation) {
+
+        double total = 0;
+        String op = operation.toLowerCase();
+
+        switch (op) {
+            case "add":
+                total = num1 + num2;
+                break;
+            case "subtract":
+                total = num1 - num2;
+                break;
+            case "multiply":
+                total = num1 * num2;
+                break;
+            case "divide":
+                if (num2 == 0) {
+                    return Map.of("operation", "divide", "warning", "Cannot divide by zero");
+                }
+                total = num1 / num2;
+                break;
+            default:
+                return Map.of("error", "Invalid operation");
+        }
+
+        return Map.of("operation", op, "total", total);
+    }
+
 }
